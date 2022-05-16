7ec5801d1a213720cd6ee1719bce9b845187e94f
Christian Lopez <chrisprodev@gmail.com>
2022-05-16 10:45:22 -0700

    Refactor Folder Structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import NavBar from "../src/NavBar";
+import NavBar from "../src/components/NavBar";
 
 const Home: NextPage = () => {
   return (
