3cd72dad3d2f762924598754c650ef0274e734fd
Christian Lopez <chrisprodev@gmail.com>
2022-05-16 08:52:34 -0700

    Initial Layout

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>NFT Landing Page</title>
+        <meta name="description" content="NFT Website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main></main>
+    </>
+  );
+};
+
+export default Home;
