3751c92f7e3350430f4d91ac4122307ffa187f09
Christian Lopez <chrisprodev@gmail.com>
2022-05-25 09:20:03 -0500

    Add Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import * as Styles from "./Styles";
+
+export const Header = () => {
+  return (
+    <Styles.Container>
+      <Styles.Title>
+        Discover, Collect, and Sell Rare NFTs.
+      </Styles.Title>
+      <p></p>
+    </Styles.Container>
+  );
+};
---
+++ src/components/Header/Styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  max-width: 100rem;
+  margin: 12rem auto;
+`;
+
+export const Title = styled.h1`
+  font-size: 11rem;
+  margin: 0;
+  text-align: center;
+`;
